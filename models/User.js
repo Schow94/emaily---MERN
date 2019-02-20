c5f48d5a15ee6a65e5f05aecac1d23f7392d94fe
Schow94 <Schow94@gmail.com>
2019-02-20 08:21:31 -0800

    Initial Github Commit

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// const Schema = mongoose.Schema
+// ^^ destructuring - Same thing
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
